92b1173c01da4b2340935733cce2144c05321add
ITSGABAY <99762916+ITSGABAY@users.noreply.github.com>
2023-07-02 23:45:32 +0300

    Make Post Final css for Today

---
+++ client/src/Pages/PostMain.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Post from "./Post";
+
+function PostMain() {
+  return (
+    <div>
+      <div className="PageDiv">
+        <div id="PostMainContainer">
+          <Post />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PostMain;
